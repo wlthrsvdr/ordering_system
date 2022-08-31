2a9919f5b20928b9eb0d3134d91c0635f85ae374
Walther Saavedra <walthersaavedra.dev@gmail.com>
2022-08-31 21:33:52 +0800

    change header

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -24,7 +24,7 @@
 
             <div class="row">
                 <div class="col-sm-12">
-                    <h3>User Count</h3>
+                    <h3>Users</h3>
                 </div>
 
             </div>
