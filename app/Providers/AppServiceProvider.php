284ef08fecd87349d5404dea520bec4f704b1c7b
Walther Saavedra <walthersaavedra.dev@gmail.com>
2022-10-08 15:20:02 +0800

    sad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+
+        $this->app->bind('path.public', function () {
+            return base_path() . '/public';
+        });
     }
 
     /**
