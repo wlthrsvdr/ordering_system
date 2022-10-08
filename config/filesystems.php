f517a0f74c694073a064aa8d3d7bd635f937c593
Walther Saavedra <walthersaavedra.dev@gmail.com>
2022-10-08 15:11:56 +0800

    fieystsem

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,9 +50,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            // 'root' => storage_path('app/public'),
-            'root' => public_path(),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('app/public'),
+            // 'root' => public_path(),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
