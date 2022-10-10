2ddc9d574200b84d5fbcfb3c1fb42449abd25950
Walther Saavedra <walthersaavedra.dev@gmail.com>
2022-10-10 21:56:07 +0800

    app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@
 */
 
 $app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+    // $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+    $this->basePath . DIRECTORY_SEPARATOR . 'public'
 );
 
 $app->bind('path.public', function () {
