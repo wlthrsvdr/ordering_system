eed2e7c01a576b0aba8560e75b7b29f8207701a4
Walther Saavedra <walthersaavedra.dev@gmail.com>
2022-10-14 16:20:29 +0800

    asd

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,8 +58,8 @@ return [
         'public' => [
             'driver' => 'local',
             // 'root' => storage_path('app/public'),
-            'root' =>    public_path(),
-            // 'url' => env('APP_URL') . '/storage',
+            'root' =>    storage_path(),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
